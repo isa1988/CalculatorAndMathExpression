858ce71f1ebd8721f4f31cd3ccca81680897d0d9
Isa Ibragimov <isa.ibragimov1988@gmail.com>
2019-08-26 22:46:34 +0600

    Добавлен веб проект на ASP.Net mvc

---
+++ WebColumnDecision/Models/PageInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebColumnDecision.Models
+{
+    /// <summary>
+    /// Любая страница
+    /// </summary>
+    public class PageInfo
+    {
+        /// <summary>
+        /// Наименование
+        /// </summary>
+        public string Title { get; set; }
+    }
+}
